c832b78195d7da9a06cb2c665e397ef1d89dca76
Mark Clark <markjclark@outlook.com>
2023-09-19 11:52:02 -0400

    Added Swagger;

--- OrchardCore.Mvc.WebApiWithWolverine/Program.cs
+++ OrchardCore.Mvc.WebApiWithWolverine/Program.cs
@@ -1,16 +1,36 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddOrchardCore()
     .AddMvc()
-// // Orchard Specific Pipeline
-// .ConfigureServices( services => {
+     // Orchard Specific Pipeline
+     .ConfigureServices( services => {
 
-// })
-// .Configure( (app, routes, services) => {
+         services.AddSwaggerGen(c =>
+         {
+             c.EnableAnnotations();
+             c.SwaggerDoc("v1", new OpenApiInfo
+             {
+                 Version = "1.0.0",
+                 Title = "OrchardCore.Mvc.WebApiWothWolverine"
+             });
+         });
+     })
+     .Configure( (app, routes, services) => {
+         var env = app.ApplicationServices.GetService<IWebHostEnvironment>();
+         env ??= app.ApplicationServices.GetService<IHostEnvironment>() as IWebHostEnvironment;
 
-// })
-;
+         if (env?.IsDevelopment() ?? false)
+         {
+             app.UseSwagger();
+             app.UseSwaggerUI(c =>
+             {
+                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "An ASP.NET Core Web API");
+             });
+         }
+     });
 
 var app = builder.Build();
 
